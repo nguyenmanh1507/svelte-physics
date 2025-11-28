eda5e3b729a147e48c5387ddd53f4a48287d7c5f
Ng Manh <nguyenmanh1507@gmail.com>
2025-11-28 12:57:25 +0000

    chore: update styles layout page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-	
+
 	let { children } = $props();
 </script>
 
@@ -9,4 +9,25 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<div class="navbar bg-neutral shadow-sm text-neutral-content">
+  <div class="flex-1">
+    <a href="/" class="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div class="flex-none">
+    <ul class="menu menu-horizontal px-1">
+      <li><a href="/">Home</a></li>
+      <li>
+        <details>
+          <summary>About</summary>
+          <ul class="bg-neutral rounded-t-none p-2">
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+        </details>
+      </li>
+    </ul>
+  </div>
+</div>
+<main class="container mx-auto py-4 px-4 md:px-0">
+  {@render children()}
+</main>
