98b5260e9cadeb2f9c352e38a796ea4c1c342b01
Ng Manh <nguyenmanh1507@gmail.com>
2025-11-30 06:49:22 +0000

    chore: add code to code highlight

--- src/routes/ten-min-physics/02-cannon-3D/+page.svelte
+++ src/routes/ten-min-physics/02-cannon-3D/+page.svelte
@@ -225,7 +225,120 @@
 
 	<CodeHighlight
 		code={`
+let threeScene: THREE.Scene
+let renderer: THREE.WebGLRenderer
+let camera: THREE.PerspectiveCamera
+let cameraControl: OrbitControls
 
+function initThreeScene() {
+	threeScene = new THREE.Scene()
+
+	// Lights
+	threeScene.add(new THREE.AmbientLight(0x505050))
+	threeScene.fog = new THREE.Fog(0x000000, 0, 15)
+
+	const spotLight = new THREE.SpotLight(0xffffff, 50)
+	spotLight.angle = Math.PI / 5
+	spotLight.penumbra = 0.2
+	spotLight.position.set(2, 3, 3)
+	spotLight.castShadow = true
+	spotLight.shadow.camera.near = 3
+	spotLight.shadow.camera.far = 10
+	spotLight.shadow.mapSize.width = 1024
+	spotLight.shadow.mapSize.height = 1024
+	threeScene.add(spotLight)
+
+	const dirLight = new THREE.DirectionalLight(0x55505a, 1)
+	dirLight.position.set(0, 3, 0)
+	dirLight.castShadow = true
+	dirLight.shadow.camera.near = 1
+	dirLight.shadow.camera.far = 10
+
+	dirLight.shadow.camera.right = 1
+	dirLight.shadow.camera.left = -1
+	dirLight.shadow.camera.top = 1
+	dirLight.shadow.camera.bottom = -1
+
+	dirLight.shadow.mapSize.width = 1024
+	dirLight.shadow.mapSize.height = 1024
+	threeScene.add(dirLight)
+
+	// Geometry
+	const ground = new THREE.Mesh(
+		new THREE.PlaneGeometry(20, 20, 1, 1),
+		new THREE.MeshPhongMaterial({ color: 0xa0adaf, shininess: 150 })
+	)
+
+	ground.rotation.x = -Math.PI / 2 // rotates X/Y to X/Z
+	ground.receiveShadow = true
+	threeScene.add(ground)
+
+	const helper = new THREE.GridHelper(20, 20)
+	helper.material.opacity = 1.0
+	helper.material.transparent = true
+	helper.position.set(0, 0.002, 0)
+	threeScene.add(helper)
+
+	// Renderer
+	renderer = new THREE.WebGLRenderer()
+	renderer.shadowMap.enabled = true
+	renderer.setPixelRatio(window.devicePixelRatio)
+	renderer.setSize(container.clientWidth, container.clientHeight)
+	window.addEventListener('resize', onWindowResize, false)
+	container.appendChild(renderer.domElement)
+
+	// Camera
+	camera = new THREE.PerspectiveCamera(
+		70,
+		container.clientWidth / container.clientHeight,
+		0.01,
+		100
+	)
+	camera.position.set(0, 1, 4)
+	camera.updateMatrixWorld()
+
+	threeScene.add(camera)
+
+	cameraControl = new OrbitControls(camera, renderer.domElement)
+	cameraControl.zoomSpeed = 2.0
+	cameraControl.panSpeed = 0.4
+}
+
+function onWindowResize() {
+	if (!containerRef) return
+
+	camera.aspect = containerRef.clientWidth / containerRef.clientHeight
+	camera.updateProjectionMatrix()
+	renderer.setSize(containerRef.clientWidth, containerRef.clientHeight)
+}
+
+function initPhysics() {
+	const radius = 0.2
+	const pos = new THREE.Vector3(radius, radius, radius)
+	const vel = new THREE.Vector3(2, 5, 3)
+
+	physicsScene().objects.push(new Ball(pos, radius, vel, threeScene))
+}
+
+function simulate() {
+	if (physicsScene().paused) return
+
+	physicsScene().objects.forEach((object) => {
+		object.simulate()
+	})
+}
+
+// Main update loop: called every frame via requestAnimationFrame
+function update() {
+	simulate()
+	renderer.render(threeScene, camera)
+	cameraControl.update()
+	requestAnimationFrame(update)
+}
+
+initThreeScene()
+initPhysics()
+update()
 	`}
 	/>
 </div>
