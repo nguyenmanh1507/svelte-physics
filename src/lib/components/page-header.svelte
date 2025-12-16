7d2429af00f5e2977332dae24f1c874c47352e0e
Ng Manh <nguyenmanh1507@gmail.com>
2025-12-16 17:22:36 +0000

    chore: update dependencies

--- src/lib/components/page-header.svelte
+++ src/lib/components/page-header.svelte
@@ -90,7 +90,7 @@
 	}
 </script>
 
-<div class="navbar bg-neutral text-neutral-content shadow-sm">
+<div class="navbar bg-neutral text-neutral-content z-10 shadow-sm">
 	<div class="flex-1">
 		<a href={resolve('/')} class="btn btn-ghost text-xl">Svelte Physics</a>
 	</div>
